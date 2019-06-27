7440e01ba14332a3528faa97b1d176928d277f0a
Mike <mikealtonji@gmail.com>
2019-06-27 01:25:16 -0400

    Major Updates
    1) No longer loading from Python: Loads from CSV.
    2) Many filters included (time, delay/cancel/etc.)
    3) Bugs fixed (counts of timeseries vs barchart)

--- app.R
+++ app.R
@@ -1,16 +1,26 @@
 library(data.table)
-library(reticulate)
 library(shiny)
+library(shinyTime)
 library(DT)
 
 setwd('C:/Users/mikea/Documents/Analytics/NJ Transit/nj_transit/')
 source('C:/Users/mikea/Documents/Analytics/NJ Transit/nj_transit/funcs.R')
 
 ### Returns "data" and "phrases"
-source_python('C:/Users/mikea/Documents/Analytics/NJ Transit/nj_transit/summarize_tweets.py')
+# source_python('C:/Users/mikea/Documents/Analytics/NJ Transit/nj_transit/data_for_R.py')
+data = read.csv('C:/Users/mikea/Documents/Analytics/NJ Transit/data.csv')
+data$date = as.Date(data$date, format = "%Y-%m-%d") # date doesn't transfer correctly from Python
+data$time_train = as.ITime(strptime(data$time_train, "%l:%M%p")) # Will set date portion to today, but shouldn't matter. I just want time.
+data$reason = as.character(data$reason)
 setDT(data)
-data$date = as.Date(data$time) # date doesn't transfer correctly from Python
-                     
+
+# Find the furthest point in the past for each train-line, then grab the newest of all of those. That's as far back as we'll look.
+min.even.date = as.Date("2000-01-01")
+for (line.id in unique(data$line)) {
+  a = min(subset(data, line == line.id)$date)
+  if (a > min.even.date) {min.even.date = a}
+}
+
 " App Starts Here "
 ui <- fluidPage(
   titlePanel("NJ Transit Delays"),
@@ -18,11 +28,17 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Inputs -------------
+      checkboxGroupInput("type", "Delays, Cancelations, or Both?", choices = c("Delays" = 'delay', "Cancelations" = 'cancel'), 
+                         inline=T, selected=c('delay', 'cancel')), 
+      br(), 
       selectizeInput("line", "Line", choices = unique(data$line), multiple=T), 
       selectizeInput("station", "Station", choices = unique(data$station), multiple=T), 
-      dateRangeInput("date", "Date", min = min(data$date), max = max(data$date), start = min(data$date), end = max(data$date)), 
-      # still need time of day filter
-      # Need to add delay/cancelation view
+      dateRangeInput("date", "Date", min = min(data$date), max = max(data$date), start = min.even.date, end = max(data$date)), 
+      fluidRow(column(12, h4('Station Departure Time Filter')), align='center'), 
+      fluidRow(column(6, timeInput("time.start", "Start Time Range", seconds = FALSE)), # still need time of day FILTER.
+               column(6, timeInput("time.end", "End Time Range", seconds = FALSE, value = strptime("23:59:59", "%T"))), align = 'center' ), 
+
+      br(), 
       numericInput("n", "Number of Reasons", min=1, max=100, value=10)
     ), # End Sidebarpanel
     
@@ -39,7 +55,7 @@ ui <- fluidPage(
 " Server Code "
 server <- function(input, output) {
   
-  data.filter = reactive(filter.df(data, input$line, input$station, input$date))
+  data.filter = reactive(filter.df(data, input$line, input$station, input$date, input$time.start, input$time.end, input$type))
   output$table <- renderDT({data.filter()})
   histogram = reactive(create.hist(data.filter(), input$n))
   output$histogram <- renderPlotly({histogram() })
--- funcs.R
+++ funcs.R
@@ -4,12 +4,27 @@ Functions Code
 library(plotly)
 library(data.table)
 
-filter.df <- function(data, line.in, station.in, date.in) {
+filter.df <- function(data, line.in, station.in, date.in, time.start, time.end, types) {
   final = data
   if (length(line.in > 0))    {final = subset(final, line %in% line.in)}
   if (length(station.in > 0)) {final = subset(final, station %in% station.in)}
+  if (time.end > time.start) {
+    final = subset(final, time_train >= as.ITime(time.start))
+    final = subset(final, time_train <= as.ITime(time.end))
+  }
   final = subset(final, date >= date.in[1] & date <= date.in[2])
-  # need to add date filter next
+  
+  # Picking Delays or Cancelations
+  if (!('cancel' %in% types)) {final = subset(final, Cancel == "False")}
+  if (!('delay' %in% types))  {final = subset(final,  Delay == "False")}
+  
+  # The most recent minimum date of all selected train-lines. To give an even starting point.
+  maximum = as.Date("2000-01-01")
+  for (line.id in unique(final$line)) {
+    a = min(subset(final, line == line.id)$date)
+    if (a > maximum) {maximum = a}
+  }
+  final = subset(final, date >= maximum)
   
   return(final)
 }
@@ -29,26 +44,43 @@ create.hist <- function(data, n) {
   reason_counts = reason_counts[order(-rank(N))]
   reason_counts = reason_counts[1:n, ]
   
-  histogram <- plot_ly(x = reason_counts$reason, y = reason_counts$N, type = "bar")
+  histogram <- plot_ly(source = 'hover_bar', x = reason_counts$reason, y = reason_counts$N, type = "bar") %>% 
+    layout(title = "Most Common Reasons for Delays")
+  
+  
   return(histogram)
 }
 
 
 create.time <- function(data) {
+  
+  # Subset data that's been hovered over
+  hover_data  <- event_data("plotly_hover", source = "hover_bar")
+  select_data <- event_data("plotly_click", source = "hover_bar")
+  
+  
+  ### ISSUE!!! # on bar graph > # cumulatively displayed...why??? block number issue??? idk!!!
+  phrases = data[, c('block_number', 'reason')]
+  setkey(phrases, "block_number")
+  phrases = unique(phrases)
+  
+  block.hover = subset(phrases, reason %in%  hover_data$x)$block_number[1]
+  block.click = subset(phrases, reason %in% select_data$x)$block_number[1]
+  
+  if (!is.null(hover_data) & !(is.null(select_data))) {
+    data = subset(data, block_number == block.hover | block_number == block.click)
+  } else if (!is.null(hover_data)) {
+    data = subset(data, block_number == block.hover)
+  } else if (!is.null(select_data)) {
+    data = subset(data, block_number == block.click)}
+  
   keycol <-c("date")
   setorderv(data, keycol)
 
-  # The most recent min value for a line. To be fair.
-  maximum = as.Date("2000-01-01")
-  for (line.id in unique(data$line)) {
-    a = min(subset(data, line == line.id)$date)
-    if (a > maximum) {maximum = a}
-  }
-  data = subset(data, date >= maximum)
-  
   data$cum = seq(from=1, to = nrow(data))
   
-  time.series <- plot_ly(x = data$date, y = data$cum, type = 'scatter', mode = "lines")
+  time.series <- plot_ly(x = data$date, y = data$cum, type = 'scatter', mode = "lines") %>% 
+    layout(title = "Delay Trends Over Time")
   return(time.series)
 }
 
