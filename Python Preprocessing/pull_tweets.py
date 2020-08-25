44a76214df4cca658388adc1a0bd6b6f01389cf1
Mike Altonji <mikealtonji@gmail.com>
2020-08-25 11:36:46 -0400

    Cleaned pull_tweets.py
    Cleaned pull_tweets.py

--- Python Preprocessing/pull_tweets.py
+++ Python Preprocessing/pull_tweets.py
@@ -6,36 +6,38 @@ Purpose: To collect the most recent Twitter data from NJ Transit accounts.
 @author: mike
 """
 
+# Import libraries
 import pandas as pd
 import tweepy
 import yaml
 import os
 
-
-"""
-Authentication with Twitter API
-"""
-
+# Load config file containing tokens, secrets and paths
 with open('config.yml') as file:
     config = yaml.full_load(file)
-
 consumer_token  = config['CONSUMER_TOKEN']
 consumer_secret = config['CONSUMER_SECRET']
 access_token    = config['ACCESS_TOKEN']
 access_secret   = config['ACCESS_SECRET']
 tweet_logs_path = config['TWEET_LOGS_PATH']
 
+# Authentication with Twitter API
 auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 
-"""  
-Function to extract tweets 
-"""
-def get_tweets(username, prior_tweets_exist = False, all_tweets = None, since_id = 0): 
+def get_tweets(username, prior_tweets_exist=False, all_tweets=None, since_id=0): 
+    """
+    Creates/Updates dataframe of Tweets for a given username
+    username: Twitter handle to scrape
+    prior_tweets_exist: True if there is already a .csv file with Tweets from this username
+    all_tweets: The dataframe of all prior Tweets for this username
+    since_id: Tweet ID of the most recent Tweet from this username in the locally stored .csv
+    """
+        
     print(username)
-    ### Get first round of tweets
+    ### First round of tweet extraction
     # Extract tweets
     if prior_tweets_exist == False:
         tweets = api.user_timeline(screen_name = username, count = 200, tweet_mode='extended') 
@@ -48,12 +50,15 @@ def get_tweets(username, prior_tweets_exist = False, all_tweets = None, since_id
     else: 
         tweets  = all_tweets # To make length work
         last_id = None
+    
+    ### Remaining rounds of tweet extraction
     # Stop running program when out of tweets OR reached 3200 tweet limit
     x = 0
     while len(tweets) > 0:
         x+=1
         print(x)
         try:
+            # Extract tweets
             tweets = api.user_timeline(screen_name = username, count = 200, max_id = last_id, since_id = since_id, tweet_mode='extended')
             # Lists for tweet text, date
             tweet_text = [tweet.full_text for tweet in tweets]
@@ -67,9 +72,7 @@ def get_tweets(username, prior_tweets_exist = False, all_tweets = None, since_id
     return(all_tweets) 
 
 
-"""
-Pull Tweets from select users, then save.
-"""
+### Pull Tweets from select users, then save.
 for name in ['NJTransit', 'NJTransit_ACRL', 'NJTransit_RVL', 'NJTransit_PVL', 'NJTransit_MBPJ', 'NJTransit_MOBO', 'NJTransit_ME', 'NJTransit_NJCL', 'NJTransit_NEC']:
     if os.path.isfile(tweet_logs_path + name + '.csv'):
         prior_tweets = pd.read_csv(tweet_logs_path + name + '.csv')
@@ -79,5 +82,3 @@ for name in ['NJTransit', 'NJTransit_ACRL', 'NJTransit_RVL', 'NJTransit_PVL', 'N
         print('First time pulling', name)
         all_tweets = get_tweets(name, False, None, None)
     all_tweets.to_csv(tweet_logs_path + name + '.csv', index=False)
-    
-    
