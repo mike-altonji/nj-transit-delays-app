8543b68ad9dec6559373d07c9026f97de387c963
Mike Altonji <mikealtonji@gmail.com>
2020-08-25 11:37:13 -0400

    Cleaned format_stations.py

--- Python Preprocessing/format_stations.py
+++ Python Preprocessing/format_stations.py
@@ -5,15 +5,22 @@ Purpose: To ensure proper labeling of train station names
 @author: mikea
 """
 
+# Import libraries
 import csv
 import pandas as pd
 import yaml
 
+# Load config containing paths
 with open('config.yml') as file:
     config = yaml.full_load(file)
 repo_path = config['REPO_PATH']
 
 def ldist(s1, s2):
+    """
+    Finds the textual distance between two strings.
+    In our case, it is a comparison between a set of pre-existing station names
+    versus raw station names in tweets. Allows for misspelled station names.
+    """
     if len(s1) > len(s2):
         s1, s2 = s2, s1
 
@@ -29,6 +36,11 @@ def ldist(s1, s2):
     return distances[-1]
 
 def right_station(text, stations):
+    """
+    Matches known station names to raw station names from tweets.
+    Leverages 'ldist' function to make string comparisons, but also contains
+    some heuristic logic based on common station abbreviations.
+    """
     if text in ['hob', 'nyps', 'nps', 'sec', 'aberdeen', 'matawan']:
         if text == 'hob': selection = 'hoboken'
         elif text == 'nyps': selection = 'psny'
@@ -46,7 +58,7 @@ def right_station(text, stations):
                 selection = station
     return selection
 
-
+# Create CSV File containing all valid station names
 stations = []
 with open(repo_path + 'valid_stations.csv', 'r') as f:
     reader = csv.reader(f)
@@ -54,7 +66,7 @@ with open(repo_path + 'valid_stations.csv', 'r') as f:
 for station in lst:
     stations.append(station[0])
 
-
+# Create CSV File containing all mappings of misspelt to correct station names, for manual validation
 wrongs = []
 with open(repo_path + 'testing.csv', 'r') as f:
     reader = csv.reader(f)
@@ -66,8 +78,8 @@ check = []
 for wrong in wrongs:
     check.append([wrong, right_station(wrong, stations)])
 
-
-station_df = pd.read_csv(repo_path + 'Station_Names.csv')
-station_df['Pred'] = station_df['Station'].apply(lambda x: right_station(x, stations))
-station_df['good'] = station_df['Actual'] == station_df['Pred']
-correct_pct = station_df[station_df['good'] == True]['Count'].sum() / station_df['Count'].sum()
+## Computes accuracy of station name predictions based on manual labelling
+#station_df = pd.read_csv(repo_path + 'Station_Names.csv')
+#station_df['Pred'] = station_df['Station'].apply(lambda x: right_station(x, stations))
+#station_df['good'] = station_df['Actual'] == station_df['Pred']
+#correct_pct = station_df[station_df['good'] == True]['Count'].sum() / station_df['Count'].sum()
